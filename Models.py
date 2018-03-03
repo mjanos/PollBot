96ae9f4b01780da16150e708d346ddddcee9b5db
Michael Janos <spikeybadooks@gmail.com>
2018-03-02 21:41:49 -0500

    Added ability to close polls, added poll author and deleting of original poll post and added ability to use 2-4 answers

--- Models.py
+++ Models.py
@@ -1,6 +1,6 @@
 import psycopg2
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String,JSON
+from sqlalchemy import Column, Integer, String,JSON, Boolean
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -27,19 +27,27 @@ class Poll(Base):
     three_voters = Column(JSON)
     four = Column(Integer,default=0)
     four_voters = Column(JSON)
+    author = Column(String(100),nullable=True)
+    closed = Column(Boolean,default=False)
 
     def generate_text(self):
-        choice_text = "`%.3d%% (%s)` 1⃣: %s\n`%.3d%% (%s)` 2⃣: %s\n`%.3d%% (%s)` 3⃣: %s\n`%.3d%% (%s)` 4⃣: %s" % (safe_div(self.one,len(self.voters)),
+        if self.closed:
+            closed_text = " CLOSED"
+        else:
+            closed_text = ""
+        choice_text = "`%.3d%% (%s)` 1⃣: %s\n`%.3d%% (%s)` 2⃣: %s\n" % (safe_div(self.one,len(self.voters)),
                                                                                 self.one,
                                                                                 self.choices[0],
                                                                                 safe_div(self.two,len(self.voters)),
                                                                                 self.two,
-                                                                                self.choices[1],
-                                                                                safe_div(self.three,len(self.voters)),
-                                                                                self.three,
-                                                                                self.choices[2],
-                                                                                safe_div(self.four,len(self.voters)),
-                                                                                self.four,
-                                                                                self.choices[3])
-        full_msg = "`Poll p" + str(self.id) + "`\n" + self.poll_message + "\n" + choice_text + "\n"
+                                                                                self.choices[1])
+        if self.choices[2]:
+            choice_text += "`%.3d%% (%s)` 3⃣: %s" % (safe_div(self.three,len(self.voters)),
+                                self.three,
+                                self.choices[2])
+        if self.choices[3]:
+            choice_text += "\n`%.3d%% (%s)` 4⃣: %s" % (safe_div(self.four,len(self.voters)),
+                                self.four,
+                                self.choices[3])
+        full_msg = "`Poll p" + str(self.id) + closed_text + "`\n" + self.poll_message + "\n" + choice_text + "\n\n`Created by " + self.author + "`"
         return full_msg
--- PollBot.py
+++ PollBot.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, Integer, String,JSON
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Models import Base, Poll
-from Settings import POLLBOT_KEY
+from Settings import POLLBOTTEST_KEY
 
 
 class PollBot(discord.Client):
@@ -36,12 +36,9 @@ class PollBot(discord.Client):
                     self.messages.append(msg)
 
     async def on_reaction_add(self,reaction,user):
-        print("got reaction")
-        print(reaction.message.author)
-        print(self.user)
         if reaction.message.author == self.user and user != self.user:
-            test_poll = test_poll = self.session.query(Poll).filter_by(message_obj=str(reaction.message.id)).one()
-            if reaction.emoji == "1⃣" and test_poll:
+            test_poll = self.session.query(Poll).filter_by(message_obj=str(reaction.message.id)).one()
+            if reaction.emoji == "1⃣" and test_poll and test_poll.choices[0].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters:
                     if str(user) in test_poll.two_voters:
                         test_poll.two_voters = [u for u in test_poll.two_voters if u != str(user)]
@@ -59,7 +56,7 @@ class PollBot(discord.Client):
                     test_poll.voters = test_poll.voters + [str(user)]
                     test_poll.one_voters = test_poll.one_voters + [str(user)]
                     test_poll.one += 1
-            elif reaction.emoji == "2⃣" and test_poll:
+            elif reaction.emoji == "2⃣" and test_poll and test_poll.choices[1].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters:
                     if str(user) in test_poll.one_voters:
                         test_poll.one_voters = [u for u in test_poll.one_voters if u != str(user)]
@@ -77,7 +74,7 @@ class PollBot(discord.Client):
                     test_poll.voters = test_poll.voters + [str(user)]
                     test_poll.two_voters = test_poll.two_voters + [str(user)]
                     test_poll.two += 1
-            elif reaction.emoji == "3⃣" and test_poll:
+            elif reaction.emoji == "3⃣" and test_poll and test_poll.choices[2].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters:
                     if str(user) in test_poll.one_voters:
                         test_poll.one_voters = [u for u in test_poll.one_voters if u != str(user)]
@@ -95,7 +92,7 @@ class PollBot(discord.Client):
                     test_poll.voters = test_poll.voters + [str(user)]
                     test_poll.three_voters = test_poll.three_voters + [str(user)]
                     test_poll.three += 1
-            elif reaction.emoji == "4⃣" and test_poll:
+            elif reaction.emoji == "4⃣" and test_poll and test_poll.choices[3].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters:
                     if str(user) in test_poll.one_voters:
                         test_poll.one_voters = [u for u in test_poll.one_voters if u != str(user)]
@@ -114,32 +111,28 @@ class PollBot(discord.Client):
                     test_poll.four_voters = test_poll.four_voters + [str(user)]
                     test_poll.four += 1
             if test_poll:
-                print("poll found")
                 self.session.commit()
                 await self.edit_message(reaction.message,test_poll.generate_text())
-            else:
-                print("poll NOT found")
-
 
     async def on_reaction_remove(self,reaction,user):
         if reaction.message.author == self.user and user != self.user:
             test_poll = self.session.query(Poll).filter_by(message_obj=str(reaction.message.id)).one()
-            if reaction.emoji == "1⃣" and test_poll:
+            if reaction.emoji == "1⃣" and test_poll and test_poll.choices[0].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters and str(user) in test_poll.one_voters:
                     test_poll.voters = [u for u in test_poll.voters if u != str(user)]
                     test_poll.one_voters = [u for u in test_poll.one_voters if u != str(user)]
                     test_poll.one -= 1
-            elif reaction.emoji == "2⃣" and test_poll:
+            elif reaction.emoji == "2⃣" and test_poll and test_poll.choices[1].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters and str(user) in test_poll.two_voters:
                     test_poll.voters = [u for u in test_poll.voters if u != str(user)]
                     test_poll.two_voters = [u for u in test_poll.two_voters if u != str(user)]
                     test_poll.two -= 1
-            elif reaction.emoji == "3⃣" and test_poll:
+            elif reaction.emoji == "3⃣" and test_poll and test_poll.choices[2].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters and str(user) in test_poll.three_voters:
                     test_poll.voters = [u for u in test_poll.voters if u != str(user)]
                     test_poll.three_voters = [u for u in test_poll.three_voters if u != str(user)]
                     test_poll.three -= 1
-            elif reaction.emoji == "4⃣" and test_poll:
+            elif reaction.emoji == "4⃣" and test_poll and test_poll.choices[3].strip() and not test_poll.closed:
                 if str(user) in test_poll.voters and str(user) in test_poll.four_voters:
                     test_poll.voters = [u for u in test_poll.voters if u != str(user)]
                     test_poll.four_voters = [u for u in test_poll.four_voters if u != str(user)]
@@ -150,7 +143,7 @@ class PollBot(discord.Client):
 
     async def on_message(self,message):
         if self.user in message.mentions:
-            reg = re.match(r'.*?"(.+?)"\s*?"(.+?)"\s*?"(.*?)"\s*?"(.*?)"\s*?"(.+?)"',message.content)
+            reg = re.match(r'.*?"(.+?)"\s"(.+?)"\s"(.*?)"(?:\s"(.*?)")?(?:\s"(.+?)")?',message.content)
             poll_msg = ""
             choice1 = ""
             choice2 = ""
@@ -175,7 +168,8 @@ class PollBot(discord.Client):
                                     one_voters=[],
                                     two_voters=[],
                                     three_voters=[],
-                                    four_voters=[]
+                                    four_voters=[],
+                                    author=str(message.author)
                                     )
                 self.session.add(new_test_poll)
                 self.session.commit()
@@ -185,24 +179,32 @@ class PollBot(discord.Client):
 
                 await self.add_reaction(msg,"1⃣")
                 await self.add_reaction(msg,"2⃣")
-                await self.add_reaction(msg,"3⃣")
-                await self.add_reaction(msg,"4⃣")
+                if choice3:
+                    await self.add_reaction(msg,"3⃣")
+                if choice4:
+                    await self.add_reaction(msg,"4⃣")
+                await self.delete_message(message)
             else:
                 reg = None
-                reg = re.match(r'.*?\sp([0-9]+)$',message.content)
+                reg = re.match(r'.*?\sp([0-9]+) close$',message.content, re.IGNORECASE)
                 if reg:
                     if not reg.group(1) is None:
-                        #poll_msg += "%s\n" % reg.group(1)
-                        print("Input %s" % (reg.group(1)))
-                        msg = await self.send_message(message.channel,'Not Implemented yet but you will be able to close the poll this way.')
-
+                        poll_to_close = self.session.query(Poll).filter_by(id=reg.group(1)).one()
+                        if poll_to_close and str(poll_to_close.author) == str(message.author):
+                            poll_to_close.closed = True
+                            self.session.commit()
+                            try:
+                                poll_msg = await self.get_message(message.channel,poll_to_close.message_obj)
+                            except discord.errors.NotFound:
+                                poll_msg = None
+                            if poll_msg:
+                                await self.edit_message(poll_msg,poll_to_close.generate_text())
+                                msg = await self.send_message(message.channel,'Poll %s closed' % (reg.group(1)))
+                        else:
+                            msg = await self.send_message(message.channel,'You are not the creator of this poll')
                 else:
                     msg = await self.send_message(message.channel,'Usage:```"<Question>"\n"<answer 1>"\n"<answer 2>"\n"<answer 3>"\n"<answer 4>"```')
 
-
-
 if __name__=="__main__":
-
-
     client = PollBot()
-    client.run(POLLBOT_KEY)
+    client.run(POLLBOTTEST_KEY)
